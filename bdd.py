928661f2371cdf35507867c456ac5d11dd28c5c6
Bob Ruiken <b.ruiken@student.science.ru.nl>
2020-02-18 18:55:36 +0100

    Added Binary Decision Diagrams

---
+++ bdd.py
@@ -0,0 +1,61 @@
+class BasicEventNode:
+    """
+    Node for a BDD which is not a leaf node.
+    """
+    def __init__(self, node_true, node_false, name):
+        """
+        BasicEventNode constructor, takes a node for the "true"
+        path, a node for the "false" path and a name.
+        """
+        self.node_true = node_true
+        self.node_false = node_false
+        self.name = name
+
+
+class LeafNode:
+    """
+    LeafNodes are for leaf nodes in BDDs. They can either be True
+    or False.
+    """
+    def __init__(self, value):
+        """
+        Constructor for a LeafNode. Takes a value which can be
+        True or False.
+        """
+        self.value = value
+
+
+class BDDConstructor:
+    """
+    The BDDConstructor can translate a Fault Tree into a BDD.
+    """
+    def __init__(self, fault_tree):
+        """
+        Constructor for the BDDConstructor. Takes as an argument
+        the fault tree to be converted.
+        """
+        self.fault_tree = fault_tree
+
+    def construct_bdd(self, variable_ordering, state):
+        """
+        The construct_bdd function takes the remaining variable
+        ordering and the current state to create a new layer
+        of the final BDD.
+        This function works depth first.
+        """
+        self.fault_tree.set_states(state)
+        if self.fault_tree.apply() or not variable_ordering:
+            return LeafNode(self.fault_tree.apply())
+        else:
+            return self.construct_node(variable_ordering[0], variable_ordering[1:], state)
+
+    def construct_node(self, variable, variable_ordering, state):
+        """
+        Construct_node is a helper function to create the entire BDD.
+        It constructs a non-leaf-node.
+        """
+        state[variable] = True
+        true_node = self.construct_bdd(variable_ordering, state)
+        state[variable] = False
+        false_node = self.construct_bdd(variable_ordering, state)
+        return BasicEventNode(true_node, false_node, variable)
