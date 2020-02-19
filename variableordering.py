6cd7dce41e42b97f179442e5040d44fa928a8a45
Justin Reniers <j.reniers@student.science.ru.nl>
2020-02-19 14:31:00 +0100

    Added variable ordering abstract class and random ordering

---
+++ variableordering.py
@@ -0,0 +1,48 @@
+from random import shuffle
+
+
+class Ordering:
+    """
+    Abstract class for variable ordering.
+    Implementations should implement the "order_variables" function.
+    """
+
+    def __init__(self, ordering_type, fault_tree):
+        """
+        Constructor for a variable ordering.
+        :param ordering_type: type of the ordering (algorithm used)
+        :param fault_tree: the Fault Tree for which the variables
+                            are ordered
+        """
+        self.ordering_type = ordering_type
+        self.fault_tree = fault_tree
+
+    def order_variables(self):
+        """
+        Returns a variable ordering that just takes the order of
+        variables of in the Fault Tree.
+        """
+        return list(self.fault_tree.get_basic_events().keys())
+
+
+class RandomOrdering(Ordering):
+    """
+    RandomOrdering is the randomly chosen variable ordering.
+    """
+
+    def __init__(self, fault_tree):
+        """
+        Constructor for a RandomOrdering.
+        :param fault_tree: the Fault Tree for which the variables
+                            are ordered
+        """
+        super().__init__('Random', fault_tree)
+
+    def order_variables(self):
+        """
+
+        :return:
+        """
+        ordering = list(self.fault_tree.get_basic_events().keys())
+        shuffle(ordering)
+        return ordering
