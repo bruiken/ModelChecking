14e3a834852e0454cf24db309c61523394e27b1b
Bob Ruiken <bobruiken@gmail.com>
2020-02-19 14:50:04 +0100

    FT: Added XOR-gates.

--- faulttree.py
+++ faulttree.py
@@ -169,6 +169,44 @@ class NotGate(Gate):
         return lambda x: not list(x)[0]
 
 
+class XorGate(Gate):
+    """
+    XorGate is a gate that represents the logical XOR-gate.
+    """
+
+    def __init__(self, name, input_gates):
+        """
+        The constructor for a XorGate.
+        :param name: The name of the XorGate.
+        :param input_gates: The input gates.
+        """
+        super().__init__('XOR', name, input_gates)
+
+    @staticmethod
+    def xor_check(results):
+        """
+        Helper function to determine the result of the XOR-
+        gate. Returns True iff exactly input of the gates is
+        True.
+        """
+        found = False
+        for result in results:
+            if result:
+                if not found:
+                    found = True
+                else:
+                    return False
+        return found
+
+    def operation(self):
+        """
+        The operation for the XorGate. Uses the helper function
+        xor_check.
+        :return:
+        """
+        return self.xor_check
+
+
 class FaultTree:
     """
     The FaultTree class defines a complete FaultTree.
