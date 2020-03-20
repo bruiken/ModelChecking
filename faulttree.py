b2fc3d6c178ccda630efe3f2d5f21d92e5b28511
Bob Ruiken <bobruiken@gmail.com>
2020-03-20 10:37:58 +0100

    Forgot to add doc for the exception.

--- faulttree.py
+++ faulttree.py
@@ -5,6 +5,11 @@ import re
 
 
 class GalileoParseException(Exception):
+    """
+    GalileoParseException is an exception used when the parsing of a
+    Galileo-formatted file failed.
+    """
+
     pass
 
 
